3b69fc33e9256668505e2c440df72e187ee5f204
AttAditya <attachment.aditya@gmail.com>
2026-05-21 05:01:33 +0530

    fix: version

--- cutypy/cli/version.py
+++ cutypy/cli/version.py
@@ -1,23 +1,30 @@
-import tomllib
-
 from pathlib import Path
+
+from tomllib import load
 
 from cutypy.models.args import Args
 
 from langex.core.functions import autosig
+
+from importlib.metadata import version
 
 @autosig
 def version_interference(args: Args) -> Args:
   if not args.version_check:
     return args
 
-  pyproject = Path("pyproject.toml")
+  try:
+    installed_version = version("cutypy")
+  except Exception:
+    ROOT = Path(__file__).resolve().parents[2]
+    pyproject = ROOT / "pyproject.toml"
 
-  with pyproject.open("rb") as f:
-    data = tomllib.load(f)
+    with pyproject.open("rb") as f:
+      data = load(f)
 
-  version = data["project"]["version"]
-  print(version)
+    installed_version = data["project"]["version"]
+
+  print(f"CutyPy {installed_version}")
 
   return Args()
 
